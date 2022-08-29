fcab59a27ef43497a2eea66bde1e202e71881648
Tofame <80583148+Tofame@users.noreply.github.com>
2022-08-29 14:57:15 +0200

    Create Pacc_7.lua

---
+++ data/actions/scripts/Pacc_7.lua
@@ -0,0 +1,10 @@
+-- Gives random amount of Pacc days and shows it on chat
+function onUse(player, item, fromPosition, target, toPosition, isHotkey)
+
+local paccBylo = player:getPremiumDays()
+player:addPremiumDays(7)
+local paccJest = player:getPremiumDays()
+	player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, "Your pacc increased from: " .. paccBylo .. " days to: " .. paccJest .. " days")
+item:remove(1)
+return true
+end
